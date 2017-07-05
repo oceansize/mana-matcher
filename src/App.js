f99e9b74008fa7c89808a3135d02a48d55c47c7e
Roi Driscoll <roi.driscoll@gmail.com>
2017-07-05 21:57:47 +0100

    Resets after research phase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import HelloWorldList from './HelloWorldList';
 
 const App = () => {
   return (
     <div className="App">
-      <HelloWorldList />
     </div>);
 };
 
