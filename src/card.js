ba99c48dec3431b01cc277b204c8925bae976ff6
Roi Driscoll <roi.driscoll@gmail.com>
2017-07-11 19:07:16 +0100

    Adds card, which can have a name.

---
+++ src/card.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import cardData from '../json/AllCards-x.json';
+
+class Card extends Component {
+
+
+  getCards (card) {
+    let selectedCard = cardData[card];
+    return selectedCard.name;
+  };
+
+  render() {
+    return(
+      <div className="Card">
+        { this.getCards('Lightning Bolt') }
+      </div>
+    );
+  };
+};
+
+export default Card;
---
+++ src/card.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Card from './card';
+
+describe('Cards', () => {
+
+  const wrapper = shallow(<Card />);
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+  });
+
+  it('has a name', () => {
+    expect(wrapper.text()).toContain('Lightning Bolt');
+  });
+});
